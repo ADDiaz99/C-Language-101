bd739d1feb94df0ebc90d7225f8af88f1fa5bfec
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-12-17 11:22:20 -0500

    cambio

---
+++ testing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *name = "testing github";
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s\n", name);
+    }
+
+    return 0;
+}
